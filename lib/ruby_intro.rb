8c466defc0c66f4a187d03490f61fd17fc49aa73
Amran Haroon <haroon3rd@gmail.com>
2021-02-09 06:39:24 +0000

    Part 2 Partial

--- lib/ruby_intro.rb
+++ lib/ruby_intro.rb
@@ -33,10 +33,15 @@ end
 
 def hello(name)
   # YOUR CODE HERE
+  return "Hello, " + name.to_s
 end
 
 def starts_with_consonant? s
   # YOUR CODE HERE
+  "/^[^aeiou\W]/i".each_char{ |letter|
+  if == nil then true
+ 	else false 
+ 	end
 end
 
 def binary_multiple_of_4? s
